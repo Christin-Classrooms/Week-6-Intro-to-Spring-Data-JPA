0a5c70ce5c37275d5c488b60acb4d442485d8ea6
ChristinHumber <cpan211fall2023@gmail.com>
2026-02-19 18:29:22 -0500

    Feb 19th class code, with lab 2 files, Starter for Data JPA and Lab3

--- src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
+++ src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
@@ -11,9 +11,24 @@ import lombok.NoArgsConstructor;
 public class Fighter {
 
     private int id;
+    
+    @NotBlank(message = "Fighter name is required")
+    @Size(min=2, max=240, message="Name must be between 2 and 240 characters")
     private String name;
+    
+    @NotNull(message = "Health is required")
+    @Min(value=1001, message="Health must be more than 1000")
+    @Max(value=1499, message="Health must be less than 1500")
     private Integer health;
+    
+    @NotNull(message = "Damage is required")
+    @DecimalMin(value="0.0", inclusive=true, message="Damage must be at least 0")
+    @DecimalMax(value="99.99", inclusive=true, message="Damage must be less than 100")
     private Double damage;
+    
+    @NotNull(message = "Resistance is required")
+    @DecimalMin(value="0.0", inclusive=true, message="Resistance must be at least 0")
+    @DecimalMax(value="10.0", inclusive=true, message="Resistance must be at most 10")
     private Double resistance;
 
 }
---
+++ src/main/java/com/example/Thymeleaf/Demo/repository/PlayerRepository.java
@@ -0,0 +1,10 @@
+package com.example.Thymeleaf.Demo.repository;
+
+import com.example.Thymeleaf.Demo.Model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
+
+}
