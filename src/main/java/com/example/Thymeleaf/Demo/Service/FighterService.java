5a4aeb99d51f3dc9c7128cb2b1af16887443e57a
Samsun <n01726448@humber.ca>
2026-02-19 20:02:38 -0500

    Lab 3: Implement FighterRespository with full CRUD

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -1,34 +1,29 @@
 package com.example.Thymeleaf.Demo.Service;
 
 import com.example.Thymeleaf.Demo.Model.Fighter;
+import com.example.Thymeleaf.Demo.repository.FighterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FighterService {
 
-    
-    private List<Fighter> fighters;
-    private static int nextId = 6;
+    private final FighterRepository fighterRepository;
 
-    public FighterService() {
-        fighters = new ArrayList<>();
-        fighters.add(new Fighter(1, "Fighter1", 100, 15.5, 0.2));
-        fighters.add(new Fighter(2, "Fighter2", 120, 12.0, 0.3));
-        fighters.add(new Fighter(3, "Fighter3", 90, 18.5, 0.15));
-        fighters.add(new Fighter(4, "Fighter4", 110, 14.0, 0.25));
-        fighters.add(new Fighter(5, "Fighter5", 95, 16.5, 0.18));
+    @Autowired
+    public FighterService(FighterRepository fighterRepository) {
+        this.fighterRepository = fighterRepository;
     }
 
+    // READ: get all fighters from database
     public List<Fighter> getAllFighters() {
-        return fighters;
+        return fighterRepository.findAll();
     }
 
+    // CREATE: save fighter to database
     public void addFighter(Fighter fighter) {
-        fighter.setId(nextId++);
-        fighters.add(fighter);
+        fighterRepository.save(fighter);
     }
-
 }
---
+++ src/main/java/com/example/Thymeleaf/Demo/repository/FighterRepository.java
@@ -0,0 +1,8 @@
+package com.example.Thymeleaf.Demo.repository;
+
+
+import com.example.Thymeleaf.Demo.Model.Fighter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FighterRepository extends JpaRepository<Fighter, Long> {
+}
