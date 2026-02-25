6ef06c6059d4cf60975a82036c748ce7ce2881d0
ralphvincent905 <ralph_sampaga@yahoo.com>
2026-02-25 14:41:48 -0500

    Lab 3 complete: JPA entity, repository, service refactor

--- src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CreateFighterController {
+
     private final FighterService fighterService;
 
     public CreateFighterController(FighterService fighterService) {
@@ -24,13 +26,14 @@ public class CreateFighterController {
     }
 
     @PostMapping("/create-fighter")
-    public String createFighter(@Valid Fighter fighter, BindingResult result) {
-        if(result.hasErrors()) {
+    public String createFighter(@Valid @ModelAttribute("fighter") Fighter fighter,
+                                BindingResult result) {
+
+        if (result.hasErrors()) {
             return "CreateFighter";
         }
 
         fighterService.addFighter(fighter);
         return "redirect:/fighters";
     }
-
 }
