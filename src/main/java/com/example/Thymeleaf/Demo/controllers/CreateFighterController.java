40469496c10ac159eb25a22bd52af4b55ba1da26
Aneesa <aneesatariq2004@gmail.com>
2026-02-25 00:24:05 -0500

    Lab 3: Implement FighterRepository with full CRUD

--- src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CreateFighterController {
+
     private final FighterService fighterService;
 
     public CreateFighterController(FighterService fighterService) {
@@ -25,12 +26,12 @@ public class CreateFighterController {
 
     @PostMapping("/create-fighter")
     public String createFighter(@Valid Fighter fighter, BindingResult result) {
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             return "CreateFighter";
         }
 
-        fighterService.addFighter(fighter);
+        fighterService.save(fighter);
+
         return "redirect:/fighters";
     }
-
 }
