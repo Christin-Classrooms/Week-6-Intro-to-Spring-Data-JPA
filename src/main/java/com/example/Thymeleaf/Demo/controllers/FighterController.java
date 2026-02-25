e2587904172926841d171151e9fed86e6920cecf
Daniel Wang <danielwang@Daniels-MacBook-Air.local>
2026-02-24 23:43:15 -0500

    Lab 3: Implement FighterRepository with full CRUD

--- src/main/java/com/example/Thymeleaf/Demo/controllers/FighterController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/FighterController.java
@@ -1,12 +1,9 @@
 package com.example.Thymeleaf.Demo.controllers;
 
-import com.example.Thymeleaf.Demo.Model.Fighter;
 import com.example.Thymeleaf.Demo.Service.FighterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class FighterController {
@@ -18,12 +15,8 @@ public class FighterController {
     }
 
     @GetMapping("/fighters")
-    public String getFighters(Model model) {
-        List<Fighter> fighters = fighterService.getAllFighters();
-
-        model.addAttribute("fighters", fighters);
-        model.addAttribute("total", fighters.size());
+    public String showFighters(Model model) {
+        model.addAttribute("fighters", fighterService.getAllFighters());
         return "Fighters";
     }
-
 }
