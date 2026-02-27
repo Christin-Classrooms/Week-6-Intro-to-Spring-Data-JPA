c5b4f0a0ffede6736b78a08624068f1f7c9428a3
CL <leungc22@gmail.com>
2026-02-26 23:32:33 -0500

    Lab 3: Implement FighterRepository with full CRUD

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -1,34 +1,25 @@
 package com.example.Thymeleaf.Demo.Service;
 
-import com.example.Thymeleaf.Demo.Model.Fighter;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.Thymeleaf.Demo.Model.Fighter;
+import com.example.Thymeleaf.Demo.repository.FighterRepository;
 
 @Service
 public class FighterService {
+    private final FighterRepository fighterRepository;
 
-    
-    private List<Fighter> fighters;
-    private static int nextId = 6;
-
-    public FighterService() {
-        fighters = new ArrayList<>();
-        fighters.add(new Fighter(1, "Fighter1", 100, 15.5, 0.2));
-        fighters.add(new Fighter(2, "Fighter2", 120, 12.0, 0.3));
-        fighters.add(new Fighter(3, "Fighter3", 90, 18.5, 0.15));
-        fighters.add(new Fighter(4, "Fighter4", 110, 14.0, 0.25));
-        fighters.add(new Fighter(5, "Fighter5", 95, 16.5, 0.18));
+    public FighterService(FighterRepository fighterRepository) {
+        this.fighterRepository = fighterRepository;
     }
 
-    public List<Fighter> getAllFighters() {
-        return fighters;
+    public List<Fighter> getFighters() {
+        return fighterRepository.findAll();
     }
 
     public void addFighter(Fighter fighter) {
-        fighter.setId(nextId++);
-        fighters.add(fighter);
+        fighterRepository.save(fighter);
     }
-
 }
---
+++ src/main/java/com/example/Thymeleaf/Demo/repository/FighterRepository.java
@@ -0,0 +1,11 @@
+package com.example.Thymeleaf.Demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Thymeleaf.Demo.Model.Fighter;
+
+public interface FighterRepository extends JpaRepository<Fighter, Integer> {
+
+    
+
+}
