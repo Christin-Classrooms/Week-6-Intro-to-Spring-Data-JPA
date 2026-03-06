35550aacfef7644e5c9da3b5cccc8149bfea472c
SahibSoorSecA <n01706710@humber.ca>
2026-03-05 23:12:00 -0500

    Lab 3: Implement FighterRepository with full CRUD

--- src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/CreateFighterController.java
@@ -3,34 +3,32 @@ package com.example.Thymeleaf.Demo.controllers;
 import com.example.Thymeleaf.Demo.Model.Fighter;
 import com.example.Thymeleaf.Demo.Service.FighterService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CreateFighterController {
-    private final FighterService fighterService;
 
-    public CreateFighterController(FighterService fighterService) {
-        this.fighterService = fighterService;
-    }
+    @Autowired
+    private FighterService fighterService;
 
-    @GetMapping("/create-fighter")
-    public String showCreateFighterForm(Model model) {
+    @GetMapping("/register")
+    public String showForm(Model model) {
         model.addAttribute("fighter", new Fighter());
-        return "CreateFighter";
+        return "createFighter";
     }
 
-    @PostMapping("/create-fighter")
-    public String createFighter(@Valid Fighter fighter, BindingResult result) {
-        if(result.hasErrors()) {
-            return "CreateFighter";
+    @PostMapping("/register")
+    public String processForm(@Valid @ModelAttribute("fighter") Fighter fighter, BindingResult result) {
+        if (result.hasErrors()) {
+            return "createFighter";
         }
-
-        fighterService.addFighter(fighter);
+        fighterService.saveFighter(fighter); 
         return "redirect:/fighters";
     }
-
 }
